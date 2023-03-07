e2737b5fb53affac4f6bd958c0dc6dd65890d245
Reinaldo Nani <reinaldo@mimobr.com>
2023-03-06 23:15:56 -0300

    RNCO 06-03-2023

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: '@react-native-community',,
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  parser: 'babel-eslint',
+  rules: {
+    semi: [2, 'never'],
+    'comma-dangle': [2, 'never'],
+    camelcase: 'off',
+    'arrow-parens': 'off',
+    'react/prop-types': 'off',
+    'import/prefer-default-export': 'off',
+    'global-require': 'off',
+    'no-useless-catch': 'off',
+    'react/no-unescaped-entities': 'off',
+    'no-nested-ternary': 'off',
+    'no-unused-expressions': 'off',
+    'class-methods-use-this': 'off',
+    'consistent-return': 'off',
+    'no-param-reassign': 'off',
+    'eslintmax-len': 'off',
+  },
 };
--- src/shared/middleware/gaLogEvent.ts
+++ src/shared/middleware/gaLogEvent.ts
@@ -12,13 +12,18 @@ export const GaLogEvents = (data: GALogEventsType) => {
     console.log('1----- ', data);
 
     const appInstanceId = await analytics().getAppInstanceId();
+    const theScreenVIew = await analytics().logScreenView({
+      screen_name: "screen_name ITEM",
+      screen_class: "screen_class ITEM",
+    });
 
     await analytics().logEvent(data.event_name, {
-      date_event: data.date,
+      date_event: data.date_event,
       element_actived: data.element_actived,
       app_instance_id: appInstanceId,
     });
     console.log('app_sc_home_cta_test_2 - APP ID - ', appInstanceId);
+    console.log('app_sc_home_cta_test_2 - theScreenVIew - ', theScreenVIew);
   };
 
   return callGALogEvents;
